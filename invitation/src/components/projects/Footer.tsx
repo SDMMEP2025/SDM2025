07137053165aa02b4ffac161a7a1d8787fd51397
YewonCALLI <96384200+YewonCALLI@users.noreply.github.com>
2025-07-29 12:20:23 +0900

    [update] invitation page

--- invitation/src/components/projects/Footer.tsx
+++ invitation/src/components/projects/Footer.tsx
@@ -2,21 +2,36 @@
 
 export function Footer() {
   return (
-    <div className='absolute z-[200] bottom-0 w-full mix-blend-difference flex flex-col-reverse justify-between items-center gap-[12px] px-[61px] py-[24px] md:flex-row md:px-[40px] md:py-[12px] lg:px-[40px] lg:py-[28px]'>
+    <div className='w-full mix-blend-difference flex flex-col-reverse justify-between items-center gap-[12px] px-[61px] py-[14px] md:flex-row md:px-[40px] md:py-[12px] lg:px-[40px] lg:py-[28px]'>
       <div className="left-[40px] top-[28px] justify-start text-sm text-white text-center font-normal capitalize leading-normal md:text-left">
         © 2025 Samsung Design Membership<span className="md:hidden">.</span><br className="md:hidden" />
         <span className="hidden md:inline"> </span>All rights reserved
       </div>
       <div className='w-fit h-6 left-[1089px] top-[28px] inline-flex justify-center items-center gap-10'>
-        <div className="justify-start text-white text-sm font-medium underline uppercase text-nowrap leading-tight">
+        <a 
+          href="https://www.design.samsung.com/kr/contents/sdm/" 
+          target="_blank" 
+          rel="noopener noreferrer"
+          className="justify-start text-white text-sm font-medium underline uppercase text-nowrap leading-tight hover:opacity-80 transition-opacity"
+        >
           Official Page
-        </div>
-        <div className="justify-start text-white text-sm font-medium underline uppercase leading-tight">
+        </a>
+        <a 
+          href="https://www.instagram.com/samsungdesignmembership/" 
+          target="_blank" 
+          rel="noopener noreferrer"
+          className="justify-start text-white text-sm font-medium underline uppercase leading-tight hover:opacity-80 transition-opacity"
+        >
           Instagram
-        </div>
-        <div className="justify-start text-white text-sm font-medium underline uppercase leading-tight">
+        </a>
+        <a 
+          href="https://www.behance.net/Samsung_Design_Mem" 
+          target="_blank" 
+          rel="noopener noreferrer"
+          className="justify-start text-white text-sm font-medium underline uppercase leading-tight hover:opacity-80 transition-opacity"
+        >
           Behance
-        </div>
+        </a>
       </div>
     </div>
   )
