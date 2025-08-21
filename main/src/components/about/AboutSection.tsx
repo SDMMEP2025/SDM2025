1b931327e251c81830795d7f04a89cd437399134
YewonCALLI <96384200+YewonCALLI@users.noreply.github.com>
2025-08-21 20:07:58 +0900

    update:about images

--- main/src/components/about/AboutSection.tsx
+++ main/src/components/about/AboutSection.tsx
@@ -104,6 +104,7 @@ export default function AboutSection({ embedded = false }: Props) {
           muted
           loop
           position='absolute'
+          aspect='aspect-[1440/1500]'
         />
       </div>
       <HeaderBody
