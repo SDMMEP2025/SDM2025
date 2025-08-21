c94fcceec885c429a04bd6f4386f81adbb2592f8
YewonCALLI <96384200+YewonCALLI@users.noreply.github.com>
2025-08-22 08:44:30 +0900

    [update] ga tag

--- main/src/components/Analytics.tsx
+++ main/src/components/Analytics.tsx
@@ -1,26 +1,20 @@
-import { GTM_ID } from "@/lib/gtag"
 import Script from "next/script";
 
 export function Analytics() {
   return (
     <>
-      {/* <!-- Google Tag Manager (noscript) --> */}
-      <noscript>
-        <iframe
-          src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
-          height="0"
-          width="0"
-          className="hidden invisible"
-        ></iframe>
-      </noscript>
-      {/* <!-- End Google Tag Manager (noscript) --> */}
-      {/* <!-- Google Tag Manager --> */}
-      <Script id="google-tag-management">{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer',"${GTM_ID}");
-
-`}</Script>
-</>
-)}
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX`}
+        strategy="afterInteractive"
+      />
+      <Script id="ga4-init" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-ZCB655NDY9');
+        `}
+      </Script>
+    </>
+  );
+}
