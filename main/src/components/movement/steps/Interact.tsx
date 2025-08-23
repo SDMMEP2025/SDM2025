f2968cd5ca0bef34c4573f9852ec71b9a223509e
YewonCALLI <96384200+YewonCALLI@users.noreply.github.com>
2025-08-23 14:21:30 +0900

    [fixed] error gyro

--- main/src/components/movement/steps/Interact.tsx
+++ main/src/components/movement/steps/Interact.tsx
@@ -110,7 +110,7 @@ export function InteractPage({ interactionData, onStartOver }: InteractPageProps
   //이곳에서 변수 수정
   const [motionParams, setMotionParams] = useState<InteractMotionParams>({
     colorInterpolationPower: 0.9,
-    tiltSensitivity: 20,
+    tiltSensitivity: 24,
     hoverScale: 1.1,
     shadowIntensity: 0,
     borderRadiusOuter: 8,
